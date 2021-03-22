f3d641cbe16ee651e1a62b3fc882c841b2246700
Massimo Pavoni <maspavoni@gmail.com>
2021-03-22 18:30:25 +0100

    Stable changes, testing Heroku deploy shortly
    - gitignore with testing bat
    - bot info and useful things in config json, together with messages and better descriptions
    - updated core extension (mortadella will be in the next few minor versions) with briefs (first patch will have detailed descriptions) and better commands
    - utf-8 for config json (also in previous commit, inside main) also in core
    - better console info for debug and project info
    - updated requirements (wrong discord api wrapper was being used) and venv
    
    TODO: verify Heroku deployment free hours and try to "have more"/reduce Odoaldo's uptime

--- bot/extensions/core.py
+++ bot/extensions/core.py
@@ -1,33 +1,42 @@
 from discord.ext import commands
 from json import load as load_json
 
+with open('bot/extensions/core_config.json', 'r', encoding='utf-8') as f:
+    core_config = load_json(f)
+
 
 class Core(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        with open('bot/extensions/core_config.json', 'r', encoding='utf-8') as f:
-            self.core_config = load_json(f)
 
-    @commands.command(aliases=['odoaldo'])
-    async def info(self, ctx):
-        await ctx.send(self.core_config['odoaldo_info'])
-
-    @commands.command(aliases=['chisono'])
-    async def whoami(self, ctx):
-        await ctx.send(f"You are {ctx.message.author.name}")
-
-    @commands.command(aliases=['marco'])
-    async def ping(self, ctx):
-        await ctx.send(f"🏓 {'Polo' if ctx.message.content.lower() == '.marco' else 'Pong'}! "
-                       f"with {str(round(self.bot.latency * 1000))}ms")
-
-    @commands.command(aliases=['morta', 'della', 'mortade'])
-    async def mortadella(self, ctx):
-        await ctx.send("**Inhales deeply**\n\n*Dove? Dimmelo.*")
-
-    @commands.command()
+    @commands.command(brief=core_config['clear_brief'],
+                      description=core_config['clear_description']
+                      if (core_config['clear_description'] != '') else core_config['clear_brief'])
+    @commands.has_permissions(administrator=True)
     async def clear(self, ctx, amount=3):
         await ctx.channel.purge(limit=amount)
+
+    @commands.command(aliases=['odoaldo'],
+                      brief=core_config['info_brief'],
+                      description=core_config['info_description']
+                      if (core_config['info_description'] != '') else core_config['info_brief'])
+    async def info(self, ctx):
+        await ctx.send(core_config['info'])
+
+    @commands.command(aliases=['marco'],
+                      brief=core_config['ping_brief'],
+                      description=core_config['ping_description']
+                      if (core_config['ping_description'] != '') else core_config['ping_brief'])
+    async def ping(self, ctx):
+        await ctx.send(f":ping_pong: {'Polo' if ctx.message.content.lower() == '.marco' else 'Pong'}! "
+                       f"with {str(round(self.bot.latency * 1000))}ms")
+
+    @commands.command(aliases=['chisono'],
+                      brief=core_config['whoami_brief'],
+                      description=core_config['whoami_description']
+                      if (core_config['whoami_description'] != '') else core_config['whoami_brief'])
+    async def whoami(self, ctx):
+        await ctx.send(f"You are {ctx.message.author.mention}")
 
 
 def setup(bot):
--- bot/main.py
+++ bot/main.py
@@ -19,17 +19,24 @@ async def on_ready():
     print("\n>>> Odoaldo is online")
 
 
-@bot.command(brief=bot_config['close_brief'],
+@bot.command(aliases=['shutdown'],
+             brief=bot_config['close_brief'],
              description=bot_config['close_description']
              if (bot_config['close_description'] != '') else bot_config['close_brief'])
 @commands.has_permissions(administrator=True)
 async def close(ctx):
+    await ctx.send(bot_config['close'])
     await bot.change_presence(status=discord.Status.offline)
     await bot.close()
-    print(">>> Odoaldo is offline")
+    print(">>> Odoaldo is offline\n\n\n")
 
 
 if __name__ == "__main__":
+    print(f"\n\n\n{'-' * 80}")
+    print(f"{bot_config['name']} - Version {bot_config['version']} - {bot_config['url']}")
+    print(f"licensed under {bot_config['license']} by {bot_config['author']} <{bot_config['author_email']}>")
+    print(f"{'-' * 80}")
+    print(f"\nAvailable extensions: {bot_config['extensions']}")
     print(f"Attempting to load startup extensions: {bot_config['startup_extensions']}")
     for extension in bot_config['startup_extensions']:
         try:

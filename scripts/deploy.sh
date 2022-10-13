4b640c95fe30b6b240342f8eefab7ab984163696
Massimo Pavoni <maspavoni@gmail.com>
2022-10-13 19:39:38 +0200

    And another one

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 mkdir -p $HOME/.local/bin/.odoaldo && cd $_
-curl -OOO https://raw.githubusercontent.com/massimopavoni/Odoaldo-DiscordBot/master/scripts/{docker-clean+pull.sh,docker-env.sh,odoaldo_docker-compose.yml}
+curl -OOO https://raw.githubusercontent.com/massimopavoni/Odoaldo-DiscordBot/master/scripts/{docker-clean+pull.sh,docker-deploy-env.sh,odoaldo_docker-compose.yml}
 cd .. && curl -O https://raw.githubusercontent.com/massimopavoni/Odoaldo-DiscordBot/master/scripts/odoaldo
 vim .odoaldo/docker-deploy-env.sh
 cd $HOME && printf "\nMake sure the variables you set are correct and run \"odoaldo\"\n"

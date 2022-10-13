9da2f09358eae1275dc4fc9088f976032b5467bc
Massimo Pavoni <maspavoni@gmail.com>
2022-10-13 19:28:50 +0200

    Fix scripts names and print

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/massimopavoni/Odoaldo-DiscordBot.git
 cd Odoaldo-DiscordBot
-vim scripts/docker-env.sh
-echo "\nMake sure the variables you set are correct and run \". scripts/docker-clean+build+push.sh\"\n"
+vim scripts/docker-build-env.sh
+printf "\nMake sure the variables you set are correct and run \". scripts/docker-clean+build+push.sh\"\n"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ mkdir -p $HOME/.local/bin/.odoaldo && cd $_
 curl -OOO https://raw.githubusercontent.com/massimopavoni/Odoaldo-DiscordBot/master/scripts/{docker-clean+pull.sh,docker-env.sh,odoaldo_docker-compose.yml}
 cd .. && curl -O https://raw.githubusercontent.com/massimopavoni/Odoaldo-DiscordBot/master/scripts/odoaldo
 vim .odoaldo/docker-deploy-env.sh
-cd $HOME && echo "\nMake sure the variables you set are correct and run \"odoaldo\"\n"
+cd $HOME && printf "\nMake sure the variables you set are correct and run \"odoaldo\"\n"
